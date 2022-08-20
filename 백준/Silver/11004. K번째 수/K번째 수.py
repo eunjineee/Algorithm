e1d828a7c6330d160508e358a023a26f91de9f76
eunjineee <108562895+eunjineee@users.noreply.github.com>
2022-08-20 21:04:05 +0900

    [Silver V] Title: K번째 수, Time: 6204 ms, Memory: 729344 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.py
@@ -0,0 +1,38 @@
+def merge_sort(arr):
+    def sort(low, high):
+        if high - low < 2:
+            return
+        mid = (low + high) // 2
+        sort(low, mid)
+        sort(mid, high)
+        merge(low, mid, high)
+
+    def merge(low, mid, high):
+        temp = []
+        l, h = low, mid
+
+        while l < mid and h < high:
+            if arr[l] < arr[h]:
+                temp.append(arr[l])
+                l += 1
+            else:
+                temp.append(arr[h])
+                h += 1
+
+        while l < mid:
+            temp.append(arr[l])
+            l += 1
+        while h < high:
+            temp.append(arr[h])
+            h += 1
+
+        for i in range(low, high):
+            arr[i] = temp[i - low]
+
+    return sort(0, len(arr))
+
+n, k = map(int,input().split())
+array = list(map(int,input().split()))
+
+merge_sort(array)
+print(array[k-1])
