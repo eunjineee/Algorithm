ea00fd61c28b01e10f4dc25d7abd75ae5e3b5f77
eunjineee <108562895+eunjineee@users.noreply.github.com>
2022-09-16 16:39:50 +0900

    [D4] Title: 정사각형 방, Time: 2,017 ms, Memory: 69,756 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.py
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.py
@@ -1,7 +1,7 @@
 
 from collections import deque
 
-def rec(i,j):
+def rec(a,i,j):
     queue.append((i,j))
     # visited = [[0]*a for _ in range(a)]
     # visited[i][j] = 1
@@ -10,11 +10,11 @@ def rec(i,j):
         i, j = queue.popleft()
         for di,dj in [[0,1],[1,0],[0,-1],[-1,0]]:
             ni,nj = i+di, j+dj
-            if 0 <= ni < n and 0 <= nj < n:
-                if nums[ni][nj] == nums[i][j] + 1:
-                    queue.append((ni,nj))
-                    # visited[ni][nj] = visited[i][j] + 1
-                    tutu += 1                   
+            if 0 <= ni < a and 0 <= nj < a and nums[ni][nj] == nums[i][j] + 1:
+                queue.append((ni,nj))
+                # visited[ni][nj] = visited[i][j] + 1
+                tutu += 1
+                    
     return tutu
 
 T = int(input())
@@ -22,20 +22,19 @@ T = int(input())
 for t in range(T):
     n = int(input())
     nums = [list(map(int, input().split())) for _ in range(n)]
-    ans1 = 0
-    ans2 = 0
+    total = {}
     queue = deque()
     for i in range(n):
         for j in range(n):
-            heyhey = rec(i, j)
-            if heyhey > ans1: 
-                ans1 = heyhey
-                ans2 = nums[i][j]
-            elif heyhey == ans1:
-                if ans2 > nums[i][j]:
-                    ans2 = nums[i][j]
+            heyhey = rec(n, i, j)
+            if heyhey not in total.keys():
+                total[heyhey] = nums[i][j]
+            else:
+                if total[heyhey] > nums[i][j]:
+                    total[heyhey] = nums[i][j]
 
-    # ans1 = max(total.keys())
-    # ans2 = total[ans1]
+
+    ans1 = max(total.keys())
+    ans2 = total[ans1]
     
     print(f'#{t+1} {ans2} {ans1}')
