9bc3195b8de6319159fae48569aa757eb71ccf2b
eunjineee <108562895+eunjineee@users.noreply.github.com>
2022-09-22 17:54:16 +0900

    [Silver I] Title: 회의실 배정, Time: 4256 ms, Memory: 43908 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,13 +1,13 @@
-N = int(input())
-nums = [list(map(int, input().split())) for _ in range(N)]
-nums.sort()
-nums.sort(key=lambda x:x[1])
-
-ans = 0
-end = -1
-while len(nums) != 0:
-    s, e = nums.pop(0)
-    if s >= end:
-        end = e
-        ans += 1
-print(ans)
+T = int(input())
+arr = []
+for _ in range(T):
+    a,b = map(int, input().split())
+    arr.append((b,a))
+arr.sort()
+q = arr[0][0]
+result = 1
+for i in range(1,T):
+    if arr[i][1] >= q:
+        result += 1
+        q = arr[i][0]
+print(result)
