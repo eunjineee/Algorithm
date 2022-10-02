5431d09b8eb1428832dd0675ab8065c66e635bf1
eunjineee <108562895+eunjineee@users.noreply.github.com>
2022-10-02 17:32:18 +0900

    [Silver I] Title: 지름길, Time: 84 ms, Memory: 32908 KB -BaekjoonHub

---
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -0,0 +1,33 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+def f(start):
+    q = []
+    heapq.heappush(q, (0,start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if dist > distance[now]:
+            continue
+        for j in arr[now]:
+            cost = dist + j[1]
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+                heapq.heappush(q, (cost,j[0]))
+
+n, d = map(int, input().split())
+arr = [[] for _ in range(d+1)]
+distance = [10000] * (d+1)
+
+for i in range(d):
+    arr[i].append((i+1, 1))
+
+for _ in range(n):
+    s, e, l = map(int, input().split())
+    if e > d:
+        continue
+    arr[s].append((e,l))
+
+f(0)
+print(distance[d])
