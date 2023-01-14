bc28483481691ba9e465df4bce50faee91cbec55
Eunjin Yang <108562895+eunjineee@users.noreply.github.com>
2023-01-14 22:07:13 +0900

    [Silver III] Title: N과 M (1), Time: 176 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,17 @@
+N, R = map(int, input().split())
+
+def f(i,k,r):
+    if i == r:
+        print(*p)
+    else:
+        for j in range(k):
+            if used[j] == 0:     # a[j] 가 아직 사용되지 않았으면
+                used[j] = 1     # a[j] 사용됨으로 표시
+                p[i] = a[j]     # p[i]는 a[j]로 결정
+                f(i+1, k,r)       # p[i+1] 값을 결정하러 이동
+                used[j] = 0     # a[j]를 다른 자리에서 쓸 수 있도록 해제
+
+a = [i for i in range(1,N+1)]
+used = [0] * N 
+p = [0] * R
+f(0, N, R)
