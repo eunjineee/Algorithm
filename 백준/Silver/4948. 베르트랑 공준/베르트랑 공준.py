78c45f5a3787a1f46976de0d1f40155f2ceceebf
Eunjin Yang <108562895+eunjineee@users.noreply.github.com>
2023-02-27 21:18:19 +0900

    [Silver II] Title: 베르트랑 공준, Time: 1468 ms, Memory: 35384 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,14 +1,20 @@
+#python3
 from math import sqrt
+li = [False, True]
+for i in range(2,246913):
+        for j in range(2,int(sqrt(i))+1):
+            if i % j == 0:
+                li.append(False)
+                break
+        else:
+            li.append(True)
+
 while True:
     ans = 0
     n = int(input())
     if n == 0:
         break
-    else:
-        for i in range(n+1,2*n+1):
-            for j in range(2,int(sqrt(i))+1):
-                if i % j == 0:
-                    break
-            else:
-                ans += 1
-        print(ans)
+    for k in range(n+1,2*n+1):
+        if li[k] == True:
+            ans += 1      
+    print(ans)
