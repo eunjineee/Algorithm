3821ed950b68f98b439b5b02a8057c9bf460fd11
Eunjin Yang <108562895+eunjineee@users.noreply.github.com>
2023-07-18 17:45:19 +0900

    [level 2] Title: 삼각 달팽이, Time: 17.04 ms, Memory: 149 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/README.md
+++ 프로그래머스/lv2/68645. 삼각 달팽이/README.md
@@ -1,10 +1,10 @@
 # [level 2] 삼각 달팽이 - 68645 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68645) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68645?language=java) 
 
 ### 성능 요약
 
-메모리: 42.9 MB, 시간: 292.63 ms
+메모리: 149 MB, 시간: 17.04 ms
 
 ### 구분
 
---
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    private static final int[] dx = {0, 1, -1};
+    private static final int[] dy = {1, 0, -1};
+
+    public int[] solution(int n) {
+        int[][] triangle = new int[n][n];
+        int v = 1;
+        int x = 0;
+        int y = 0;
+        int d = 0;
+
+        while (true){
+            triangle[x][y] = v++;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[nx][ny] != 0){
+                d = (d+1) % 3;
+                nx = x + dx[d];
+                ny = y + dy[d];
+                if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[nx][ny] != 0) break;
+            }
+            x = nx;
+            y = ny;
+        }
+        int[] result = new int[v-1];
+        int index = 0;
+        for (int i = 0; i < n; i ++){
+            for (int j = 0; j <= i; j++){
+                result[index++] = triangle[j][i];
+            }
+        }
+        return result;
+    }
+}
