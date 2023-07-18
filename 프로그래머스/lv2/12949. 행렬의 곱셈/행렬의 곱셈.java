6ea284e32623f26992f98047c045c7b314811389
Eunjin Yang <108562895+eunjineee@users.noreply.github.com>
2023-07-18 21:10:17 +0900

    [level 2] Title: 행렬의 곱셈, Time: 2.91 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -6,7 +6,7 @@ public class Solution {
         for (int i = 0; i < answer.length; i++) {
             for (int j = 0; j < answer[i].length; j++) {
                 // answer에서 0을 먼저 넣어야 아래에서 더하는 형식 사용할 수 있음 : 자바라서 처음에 빈리스트로 만든건지?!
-                answer[i][j] = 0;
+      
                 for (int k = 0; k < arr1[i].length; k++) {
                     answer[i][j] += arr1[i][k] * arr2[k][j];
                 }
