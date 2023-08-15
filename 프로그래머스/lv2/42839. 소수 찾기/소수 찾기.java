bbbc2b3b83a31c4876014315d1c71d3a48e4e83f
Eunjin Yang <108562895+eunjineee@users.noreply.github.com>
2023-08-15 17:13:48 +0900

    [level 2] Title: 소수 찾기, Time: 2.07 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+
+class Solution {
+    private static boolean isPrime(int n){
+        if (n<=1) return false;
+        for (int i = 2; i*i <=n; i++){
+            if (n%i ==0) return false;
+        }
+        return true;
+    }
+
+    private static void getPrimes(int acc, int[] numbers, boolean[] isUsed,Set<Integer> primes){
+        if (isPrime(acc)) primes.add(acc);
+
+        for (int i = 0; i< numbers.length; i++){
+            if(isUsed[i]) continue;
+            int nextAcc = acc * 10 + numbers[i];
+
+            isUsed[i] = true;
+            getPrimes(nextAcc, numbers, isUsed, primes);
+            isUsed[i] = false;
+        }
+    }
+
+    public static int solution(String nums) {
+        Set<Integer> primes = new HashSet<>();
+        int[] numbers = nums.chars().map(c -> c - '0').toArray();
+        getPrimes(0, numbers, new boolean[numbers.length], primes);
+        return primes.size();
+    }
+}
