1adbee19db47d8835eda7c9dff3a1e8655a3ca60
Eunjin Yang <108562895+eunjineee@users.noreply.github.com>
2023-08-29 15:36:42 +0900

    [level 0] Title: A로 B 만들기, Time: 0.13 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+class Solution {
+    public static Map<Character, Integer> toMap (String word){
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : word.toCharArray()) {
+            map.putIfAbsent(c, 0);
+            map.put(c, map.get(c) + 1);
+        }
+        return map;
+    }
+
+    public static Integer solution(String before, String after) {
+        return toMap(before).equals(toMap(after)) ? 1:0;
+    }
+}
